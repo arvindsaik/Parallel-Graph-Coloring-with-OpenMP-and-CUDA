93af818718be450858ebeead1034982058b11ed6
Arvind Saio Krishnan <arvindsk@andrew.cmu.edu>
2020-04-20 20:52:42 -0400

    Basic parallle version, bug fixes in serial

--- parallel-omp.cpp
+++ parallel-omp.cpp
@@ -1,4 +1,7 @@
 #include <bits/stdc++.h>
+#include <omp.h>
+
+
 using namespace std;
 
 void printGraph(int n, int m[], int **adj_list) {
@@ -32,18 +35,26 @@ void setGraph(int n, int m[], int **adj_list) {
 }
 
 void assign_colors(int num_conflicts, int *conflicts, int maxd, int *m, int **adj_list, int *colors) {
-	bool *forbidden = (bool *) malloc((maxd + 1) * sizeof(bool));
+	bool **forbidden = (bool **) malloc(num_conflicts * sizeof(bool *));
 	for (int i = 0; i < num_conflicts; ++i) {
-		for (int j = 0; j < maxd; ++j) {
-			forbidden[j] = false;
+		forbidden[i] = (bool *) malloc((maxd + 1) * sizeof(bool));
+	}
+#if OMP
+	#pragma omp parallel num_threads(4)
+	#pragma omp for
+#endif
+	for (int i = 0; i < num_conflicts; ++i) {
+		for (int j = 0; j < maxd + 1; ++j) {
+			forbidden[i][j] = false;
 		}
 		int v = conflicts[i];
 		for (int j = 0; j < m[v]; ++j) {
 			int u = adj_list[v][j];
-			forbidden[colors[u]] = true;
+			if (colors[u] >= 0)
+				forbidden[i][colors[u]] = true;
 		}
-		for(int j = 0; j < maxd + 1; ++j) {
-			if (forbidden[j] == false) {
+		for (int j = 0; j < maxd + 1; ++j) {
+			if (forbidden[i][j] == false) {
 				colors[v] = j;
 				break;
 			}
@@ -52,13 +63,22 @@ void assign_colors(int num_conflicts, int *conflicts, int maxd, int *m, int **ad
 }
 
 void detect_conflicts(int num_conflicts, int *conflicts, int *m, int **adj_list, int *colors,
-		int temp_num_conflicts, int *temp_conflicts) {
+                      int *temp_num_conflicts, int *temp_conflicts) {
+//	#if OMP
+//		#pragma omp parallel num_threads(4)
+//		#pragma omp for
+//	#endif
 	for (int i = 0; i < num_conflicts; ++i) {
 		int v = conflicts[i];
 		for (int j = 0; j < m[v]; ++j) {
 			int u = adj_list[v][j];
 			if (colors[u] == colors[v] && u < v) {
-				temp_conflicts[temp_num_conflicts++] = u;
+//				#if OMP
+//					#pragma omp critical
+//				#endif
+				temp_conflicts[*temp_num_conflicts] = u;
+				*temp_num_conflicts = *temp_num_conflicts + 1;
+				colors[u] = -u;
 			}
 		}
 	}
@@ -75,10 +95,9 @@ void IPGC(int n, int m[], int maxd, int **adj_list) {
 	int *temp_conflicts = (int *) malloc(num_conflicts * sizeof(int));
 
 	while (num_conflicts) {
-
 		assign_colors(num_conflicts, conflicts, maxd, m, adj_list, colors);
 
-		detect_conflicts(num_conflicts, conflicts, m, adj_list, colors, temp_num_conflicts, temp_conflicts);
+		detect_conflicts(num_conflicts, conflicts, m, adj_list, colors, &temp_num_conflicts, temp_conflicts);
 
 		// Swap
 		num_conflicts = temp_num_conflicts;
@@ -86,19 +105,20 @@ void IPGC(int n, int m[], int maxd, int **adj_list) {
 		temp = temp_conflicts;
 		temp_conflicts = conflicts;
 		conflicts = temp;
+		temp_num_conflicts = 0;
 	}
 
 	for (int i = 0; i < n; ++i) {
 		cout << "Color of node " << i << " : " << colors[i] << endl;
 	}
- }
+}
 
 int main() {
 	int nvertices = 4;
 	int num_edges[] = {2, 3, 2, 3};
 	int max_degree = 3;
-	int **adjacency_list = (int **) malloc(nvertices*sizeof(int *));
-	for (int i = 0 ; i < nvertices; ++i) {
+	int **adjacency_list = (int **) malloc(nvertices * sizeof(int *));
+	for (int i = 0; i < nvertices; ++i) {
 		adjacency_list[i] = (int *) malloc(num_edges[i] * sizeof(int));
 	}
 
