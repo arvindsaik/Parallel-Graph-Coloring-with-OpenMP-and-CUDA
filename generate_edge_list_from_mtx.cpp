71593b5d81715380ac611fc821fb08fbaa1e4681
Vilas Bhat <vilasb@andrew.cmu.edu>
2020-04-29 14:00:39 -0400

    Added generator for edge list

---
+++ generate_edge_list_from_mtx.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    string filename;
+    filename = argv[1];
+    int flag = atoi(argv[2]);
+    ifstream fin(filename.c_str());
+
+    while (fin.peek() == '%') fin.ignore(2048, '\n');
+
+    long M, N, L;
+    long edges = 0;
+    set<pair<long, long> > edges_set;
+
+    fin >> M >> N >> L;
+    vector<vector<long> > adjacency_list(M, vector<long>(0));
+    long max_degree = 0;
+    cout << "Num vertices: " << M << endl;
+
+    string new_filename = filename.substr(0, filename.length()-4);
+    new_filename += "_edge_list";
+    ofstream fout(new_filename.c_str());
+
+    for (int i = 0; i < L; i++) {
+        long a, b;
+        double d;
+        fin >> a >> b;
+        if (flag == 0) fin >> d;
+        a -= 1;
+        b -= 1;
+        if (a == b) continue;
+        if (edges_set.find(make_pair(a, b)) != edges_set.end() || edges_set.find(make_pair(b, a)) != edges_set.end()) {
+            continue;
+        } else {
+            edges_set.insert(make_pair(a, b));
+        }
+
+        fout << a << " " << b << "\n";
+    }
+
+    fout.close();
+    cout << "Format of the graph generated: " <<endl ;
+    cout << "VERTEX1 VERTEX2" << endl;
+    cout << "VERTEX3 VERTEX4" << endl;
+    cout << "VERTEX5 VERTEX6" << endl;
+    cout << "... <all edges>" << endl;
+    return 0;
+}
